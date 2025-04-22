31593d893c95f71afe5ccd388d128ddd4c284714
Fernanda <fernanda.wynter@gmail.com>
2025-04-22 15:43:02 +0200

    fixed text from be

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,4 +1,7 @@
 import User from "../schemas/userSchema.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import asyncHandler from "../utils/asyncHandler.js";
 import { CustomError } from "../utils/errorHandler.js";
 
 export const getAllUsers = async (req, res, next) => {
@@ -25,8 +28,15 @@ export const getUserById = async (req, res, next) => {
 
 export const createUser = async (req, res, next) => {
     console.log("Creating user with data:", req.body);
-  try {
-    const newUser = await User.create(req.body);
+    const { user_name, user_email, user_password } = req.body;
+    const hashedPassword = await bcrypt.hash(user_password, 10);
+    const newUser = new User({
+      user_name,
+      user_email,
+      user_password: hashedPassword, 
+    })
+    try {
+    await newUser.save();
     res.status(201).json(newUser);
   } catch (error) {
     next(new CustomError("Error creating user", 500));
@@ -59,5 +69,60 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
     next(new CustomError("Error deleting user", 500));
+  }
+};
+
+
+export const loginUser = asyncHandler(async (req, res) => {
+  const { user_email , user_password } = req.body;
+  const user = await User.findOne({ user_email });
+
+  if (!user) {
+    throw new CustomError('Invalid email or password', 401);
+  }
+
+  const isMatch = await bcrypt.compare(user_password, user.user_password);
+  if (!isMatch) {
+    throw new CustomError('Invalid email or password', 401);
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      user_name: user.user_name,
+      user_email: user.user_email,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRES_IN }
+  );
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+  res.status(200).json({
+    message: 'Login successful',
+    user: {
+      id: user._id,
+      user_name: user.user_name,
+      user_email: user.user_email,
+    },
+  });
+});
+
+// User Logout
+export const logoutUser = (req, res) => {
+  res.clearCookie('token');
+  res.status(200).json({ message: 'Logout successful' });
+};
+
+// Check Session
+export const checkSession = (req, res) => {
+  if (req.user) {
+    res.json({ authenticated: true, user: req.user });
+  } else {
+    res.json({ authenticated: false });
   }
 };
